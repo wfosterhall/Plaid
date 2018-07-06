4a99dc76e4a1a38cf70a77e60b8ad77898f6b15b
reubenjamesbishop <39641783+reubenjamesbishop@users.noreply.github.com>
2018-07-06 16:07:45 +0930

    Have a tile base now

--- js/plaid.js
+++ js/plaid.js
@@ -1,16 +1,96 @@
-//Create a scene 
-var scene = new THREE.Scene();
+//Setup
+var scene, camera, renderer;
 
-//Create a camera 
-var aspectRatio = window.innerWidth / window.innerHeight;
-var d = 20;
-camera = new THREE.OrthographicCamera( - d * aspectRatio, d * aspectRatio, d, - d, 1, 1000 );
+init();
 
-camera.position.set( 20, 20, 20 ); // all components equal
-camera.lookAt( scene.position ); // or the origin
+//Scene setup
+function init()
+{
+	//Create a scene
+	scene = new THREE.Scene();
 
 
-//Render 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+	//Renderer
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( width, height );
+	document.body.appendChild( renderer.domElement );
+
+	//Camera
+	camera = new THREE.PerspectiveCamera( 45, width/height, 0.1, 500 );
+	scene.add( camera );
+
+	//Adjust camera
+	var camDist = 100;
+	camera.position.set( camDist, camDist, camDist ); //Camera equal distance away
+	camera.lookAt( scene.position ); //Camera always looks at origin
+
+	//Add some ambient light
+	var ambient = new THREE.AmbientLight( 0x404040 );
+	scene.add( ambient );
+
+	// //Controls 
+	// var controls = new THREE.OrbitControls( camera );
+
+	render();
+}
+
+	console.log("Starting");
+
+	// //Lay down a grid 
+	// var gridSize = 10;
+	// var gridDiv = 10;
+	// var gridHelper = new THREE.GridHelper( gridSize, gridDiv );
+	// scene.add( gridHelper );
+
+	var n = 10;
+
+	for(var i = -n/2 * 10; i < n/2*10; i += 10)
+	{
+		for(var j = -n/2*10; j < n/2*10; j +=10 )
+		{
+
+			layTile(i,j);
+
+		}
+	}
+
+
+
+
+
+//Lay down base tiles 
+function layTile(x,y)
+{
+
+	//Create a tile 
+	var geometry = new THREE.BoxGeometry( 10, 2, 10 );
+
+	var col = [0x00ff00, 0x00fff0];
+
+	var thiscol = Math.floor(Math.random() * 2);
+
+	var material = new THREE.MeshLambertMaterial( {color:col[thiscol] } );
+	var baseTile = new THREE.Mesh( geometry, material );
+	
+	//Place it in the scene 
+	scene.add( baseTile );
+	baseTile.position.set(x,0,y);
+}
+
+
+//Draw/render the scene on the canvas 
+function render()
+{
+	//requestAnimationFrame(render);
+	renderer.render( scene, camera );
+};
+
+var mainLoop = function()
+{
+
+	render();
+}
+
+mainLoop();
